09e126396e8690d1f89dbd866611e5ea0e83ce2e
Chaney McIntier <cmcintier01@gmail.com>
2025-04-24 23:48:32 -0500

    Initial commit.

---
+++ src/client/java/schedule1/schedule1/Schedule1Client.java
@@ -0,0 +1,10 @@
+package schedule1.schedule1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Schedule1Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/schedule1/schedule1/Schedule1Client.java
@@ -0,0 +1,4 @@
+package schedule1.schedule1;
+
+public class Schedule1Client {
+}
