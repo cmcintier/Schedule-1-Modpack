6480562dadba5b150e7b95b837dc28fd8f2478e0
Chaney McIntier <cmcintier01@gmail.com>
2025-05-04 17:25:46 -0500

    Added Harvester functionality  -> must have harvester in hand to harvest weed plant Fixed Weed Bush  -> Main issue was using old states after state values had been updated.

--- src/main/java/schedule1/schedule1/block/custom/GrandaddyPurpleBushBlock.java
+++ src/main/java/schedule1/schedule1/block/custom/GrandaddyPurpleBushBlock.java
@@ -3,6 +3,7 @@ package schedule1.schedule1.block.custom;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
@@ -23,6 +24,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.event.GameEvent;
 import schedule1.schedule1.Schedule1;
+import schedule1.schedule1.Schedule1DataGenerator;
 import schedule1.schedule1.item.ModItems;
 
 public class GrandaddyPurpleBushBlock extends PlantBlock implements Fertilizable{
@@ -32,9 +34,9 @@ public class GrandaddyPurpleBushBlock extends PlantBlock implements Fertilizable
     public static final IntProperty AGE = Properties.AGE_4;
     private static final VoxelShape SMALL_SHAPE = Block.createCuboidShape(6.0, 0.0, 3.0, 10.0, 8.0, 10.0);
     private static final VoxelShape LARGE_SHAPE = Block.createCuboidShape(6.0, 0.0, 3.0, 10.0, 16.0, 10.0);
-    private int MAX_YIELD;
-    private int current_yield;
-    private int harvested;
+    private static final IntProperty MAX_YIELD = IntProperty.of("max_yield", 7, 12);
+    private static final IntProperty CURRENT_YIELD = IntProperty.of("current_yield", 0, 12);
+    private static final IntProperty HARVESTED = IntProperty.of("harvested", 0, 12);
 
 
     @Override
@@ -44,10 +46,10 @@ public class GrandaddyPurpleBushBlock extends PlantBlock implements Fertilizable
 
     public GrandaddyPurpleBushBlock(AbstractBlock.Settings settings) {
         super(settings);
-        this.MAX_YIELD = (int)Math.round(Math.random() * 5) + 7;
-        this.current_yield = 0;
-        this.harvested = 0;
         this.setDefaultState(this.stateManager.getDefaultState().with(AGE, 0));
+        this.setDefaultState(this.stateManager.getDefaultState().with(MAX_YIELD, 7));
+        this.setDefaultState(this.stateManager.getDefaultState().with(CURRENT_YIELD, 0));
+        this.setDefaultState(this.stateManager.getDefaultState().with(HARVESTED, 0));
     }
 
     @Override
@@ -69,54 +71,63 @@ public class GrandaddyPurpleBushBlock extends PlantBlock implements Fertilizable
         return new ItemStack(ModItems.GRANDADDY_PURPLE);
     }
 
+//    @Override
+//    protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+//      if(!world.isClient) {
+//          world.setBlockState(pos, state.with(MAX_YIELD, (int) Math.round(Math.random() * 5) + 7));
+//      }
+//    }
+
     @Override
     protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         int i = state.get(AGE);
-        boolean bl = i == 3;
-        return !bl && stack.isOf(Items.BONE_MEAL)
-                ? ItemActionResult.SKIP_DEFAULT_BLOCK_INTERACTION
-                : super.onUseWithItem(stack, state, world, pos, player, hand, hit);
-    }
-
-    @Override
-    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        int i = state.get(AGE);
-        if (i == MAX_AGE) {
+        boolean bl = i == MAX_AGE;
+        if(!bl && stack.isOf(Items.BONE_MEAL)){
+            return ItemActionResult.SKIP_DEFAULT_BLOCK_INTERACTION;
+        } else if (bl && stack.isOf(ModItems.HARVESTER)) {
             // int j = 1 + world.random.nextInt(2);
             // If max age, drop j + 1
             // dropStack(world, pos, new ItemStack(ModItems.GRANDADDY_PURPLE, j + (bl ? 1 : 0)));
-            dropStack(world, pos, new ItemStack(ModItems.GRANDADDY_PURPLE, 1));
-            if(!world.isClient) {
-                this.harvested++;
-                Schedule1.LOGGER.info(String.valueOf(this.harvested));
-                Schedule1.LOGGER.info(String.valueOf(this.MAX_YIELD));
-                Schedule1.LOGGER.info(String.valueOf(state.getBlock().toString()));
+
+            if (!world.isClient) {
+                dropStack(world, pos, new ItemStack(ModItems.GRANDADDY_PURPLE, 1));
+                // incrementing
+                world.setBlockState(pos, state.with(HARVESTED, state.get(HARVESTED) + 1));
+//                Schedule1.LOGGER.info(String.valueOf(world.getBlockState(pos).get(HARVESTED)));
+//                Schedule1.LOGGER.info(String.valueOf(world.getBlockState(pos).get(CURRENT_YIELD)));
+                //Schedule1.LOGGER.info(String.valueOf(state.getBlock().toString()));
+
+                world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
+//                Schedule1.LOGGER.info("Harvested: {} Max Yield: {}", world.getBlockState(pos).get(HARVESTED), world.getBlockState(pos).get(MAX_YIELD));
+
+                if (world.getBlockState(pos).get(HARVESTED).equals(state.get(MAX_YIELD))) {
+                    BlockState blockState = state.with(AGE, 1);
+                    world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
+                    world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+                }
             }
 
-            world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
-            if (this.harvested == this.MAX_YIELD) {
-                Schedule1.LOGGER.info("Break");
-                BlockState blockState = state.with(AGE, 1);
-                world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
-                world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
-            }
-            return ActionResult.success(world.isClient);
-        } else if (i == MAX_AGE - 1 && this.harvested <= this.current_yield) {
-            dropStack(world, pos, new ItemStack(ModItems.GRANDADDY_PURPLE, 1));
+
+            return ItemActionResult.success(world.isClient);
+        } else if (i == MAX_AGE - 1 && state.get(HARVESTED) < state.get(CURRENT_YIELD)) {
             if(!world.isClient) {
-                Schedule1.LOGGER.info("Young");
-                this.harvested++;
+                dropStack(world, pos, new ItemStack(ModItems.GRANDADDY_PURPLE, 1));
+                world.setBlockState(pos, state.with(HARVESTED, state.get(HARVESTED) + 1));
+                world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
             }
-            return ActionResult.success(world.isClient);
+                return ItemActionResult.success(world.isClient);
         }
 
-        return ActionResult.PASS;
 
+        return ItemActionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(AGE);
+        builder.add(MAX_YIELD);
+        builder.add(CURRENT_YIELD);
+        builder.add(HARVESTED);
     }
 
     @Override
@@ -131,14 +142,24 @@ public class GrandaddyPurpleBushBlock extends PlantBlock implements Fertilizable
 
     @Override
     public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+
         int i = Math.min(MAX_AGE, state.get(AGE) + 1);
-        world.setBlockState(pos, state.with(AGE, i), Block.NOTIFY_LISTENERS);
-        if(state.get(AGE) == MAX_AGE - 1) {
-            current_yield += 2;
-            Schedule1.LOGGER.info(String.valueOf("Grow stage 3 " + current_yield));
+        //AGE State (state.with(AGE, i) not updating properly and setBlockState is updating routine
+        //Why was AGE State updated inside setBlockState
+        if(!world.isClient) {
+            if (i == MAX_AGE - 1) {
+                Schedule1.LOGGER.info("[Stage3] (MAX_AGE - 1) Hit");
+                world.setBlockState(pos, state.with(CURRENT_YIELD, 2));
+            } else if (i == MAX_AGE) {
+                Schedule1.LOGGER.info("[Stage4] Max Age Hit");
+                world.setBlockState(pos, state.with(MAX_YIELD, (int)Math.round(Math.random() * 5) + 7));
+            }
+            //
+            world.setBlockState(pos, world.getBlockState(pos).with(AGE, i), Block.NOTIFY_LISTENERS);
+            // get updated state world.getBlockState(pos).get(<STATE>)
+            Schedule1.LOGGER.info("Grow stage " + world.getBlockState(pos).get(AGE) + " " + world.getBlockState(pos).get(CURRENT_YIELD));
         }
-        else if(state.get(AGE) == MAX_AGE)
-            current_yield += MAX_YIELD - current_yield;
+
     }
 }
 
